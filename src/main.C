0eba4dc3b3f68cb37af0495b243c100fcde74684
reimersa <arne.reimers@desy.de>
2018-04-20 13:15:50 +0200

    another test comment on branch test

--- src/main.C
+++ src/main.C
@@ -38,7 +38,7 @@ int main(){
   // Get training data
   // -----------------
 
-  //test comment
+  //test comment test
   
   vector<Mat<double>> inputs = InputMatrix();
   Mat<double> X = inputs[0];
